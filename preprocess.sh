406322dcfcbaeb122a9fbb6b0edeef215e438355
lzmisscc <1028741731@qq.com>
2021-02-04 19:57:17 +0800

    添加sh脚本

---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+python -m sightseq.preprocess --task text_recognition \
+    --trainpref train.txt \
+    --destdir ./ --padding-factor 1
--- train_crnn.sh
+++ train_crnn.sh
@@ -2,19 +2,25 @@
 #     --task text_recognition --arch decoder_transformer \
 #     --batch-size 16 --dropout 0.0  --max-epoch 100 \
 #     --criterion cross_entropy \
-#     --num-workers 4 --optimizer adam --decoder-layers 2 \
+#     --num-workers 4 --optimizer adam --decoder-layers 2\
 #     --adam-eps 1e-04 --lr 0.001 --min-lr 1e-09 \
 #     --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
 #     --weight-decay 0.0 --no-token-crf --no-token-rnn \
-#     --save-interval 1 --encoder-normalize-before
-python -m sightseq.train table_dataset \
+#     --save-interval 1 --encoder-normalize-before 
+python -m sightseq.train ./ \
     --task text_recognition --arch decoder_attention \
     --decoder-layers 2 --batch-size 16 --dropout 0.0 \
     --max-epoch 100 --criterion cross_entropy --num-workers 4 \
     --optimizer adam --adam-eps 1e-04 --lr 0.001 --min-lr 1e-09 \
     --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --weight-decay 0.0 \
     --no-token-crf --save-interval 1
-
+# python -m sightseq.train ./ \
+#     --task text_recognition --arch decoder_crnn \
+#     --decoder-layers 2 --batch-size 4 \
+#     --max-epoch 50 --criterion ctc_loss --num-workers 4 \
+#     --optimizer adam --adam-eps 1e-04 --lr 0.001 --min-lr 1e-09 \
+#     --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --weight-decay 0.0 \
+#     --save-interval 1
 # python ./main.py --dataset-root table_dataset --arch densenet121 \
 #     --alphabet table_dataset/dict.txt \
 #     --lr 5e-5 --optimizer rmsprop --gpu-id 2 \
